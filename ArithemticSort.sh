c8a088ce33c972a75a78c9fd5ec03fab6ed4c82b
RKSunan <rajkumarisunanda@gmail.com>
2020-11-03 15:07:49 +0530

    AritmeticComputauion

--- ArithemticSort.sh
+++ ArithemticSort.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
-echo "Enter the first value" x
-read x 
-echo "Enter the second value" y
-read y
-echo "Enter the third value" z
-read z
+echo "Enter the first value" 
+read a
+echo "Enter the second value" 
+read b
+echo "Enter the third value" 
+read c
 echo "The Three values are : "
-echo $x $y $z
+echo $a $b $c
+result=`expr $a + $b \* $c`
+echo "The result is : $result "
